289b788c0a8f005325fcc9097388c01fce6ec788
Leoul Tilahun <33269725+PrinceUmbrella@users.noreply.github.com>
2020-10-14 10:38:16 -0400

    Webpage reorg

---
+++ trivia/src/components/Questions.tsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+
+function Questions() {
+  return (
+    <div className="App">
+
+      <h1>Questions</h1>
+    </div>
+  );
+}
+
+export default Questions;
---
+++ trivia/src/components/Score.tsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+
+function Score() {
+  return (
+    <div className="App">
+
+      <h1>Score</h1>
+    </div>
+  );
+}
+
+export default Score;
