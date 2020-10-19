54536f7fd7f2b0fa4297eac2c65acd455e52a2d3
Leoul Tilahun <33269725+PrinceUmbrella@users.noreply.github.com>
2020-10-19 11:58:17 -0400

    input form in jumbotron

--- trivia/src/components/InputHandler.tsx
+++ trivia/src/components/InputHandler.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, Redirect, useHistory } from 'react-router-dom';
-import { Button, Input, InputGroup, InputGroupAddon } from 'reactstrap';
+import { Button, Input, InputGroup, InputGroupAddon, Jumbotron } from 'reactstrap';
 import {playerName } from '../redux/action';
 
 function InputHandler() {
@@ -16,18 +16,19 @@ function InputHandler() {
 
 	return (
 		<div className="App">
+            <Jumbotron>
 		    <h1>Input</h1>
-            <InputGroup>
-                <Input type="text" placeholder={"Player 1's Name"} onChange={(event) => setPlayerOneInfo(event.target.value)}/>
-                {/* <Button onClick={() => buttonClicked(0,playerOneInfo)}>Submit Name</Button> */}
-            </InputGroup>
-            <br />
-            <InputGroup>
-                <Input type="text" placeholder={"Player 2's Name"} onChange={(event) => setPlayerTwoInfo(event.target.value)}/>
-                {/* <Button onClick={() => buttonClicked(1,playerTwoInfo)}>Submit Name</Button> */}
-            </InputGroup>
-            <br />
-            <Link to="/home" onClick={() => buttonClicked(playerOneInfo, playerTwoInfo)} className="btn btn-primary">Sign up</Link>
+                <InputGroup>
+                    <Input type="text" placeholder={"Player 1's Name"} onChange={(event) => setPlayerOneInfo(event.target.value)}/>
+                </InputGroup>
+                <br />
+                <InputGroup>
+                    <Input type="text" placeholder={"Player 2's Name"} onChange={(event) => setPlayerTwoInfo(event.target.value)}/>
+                </InputGroup>
+                <br />
+                <Link to="/home" onClick={() => buttonClicked(playerOneInfo, playerTwoInfo)} className="btn btn-primary">Sign up</Link>
+            </Jumbotron>
+
 		</div>
   );
 }
