622db16171b1f140f2096f9b3eebea9222a0da24
Jussepen <33269715+Jussepen@users.noreply.github.com>
2020-10-21 22:08:34 -0400

    added incorrectQuestion array

--- trivia/src/redux/action.tsx
+++ trivia/src/redux/action.tsx
@@ -5,53 +5,62 @@ export const NEXT_QUESTION = "NEXT_QUESTION";
 export const SET_ANSWER = "SET_ANSWER";
 export const SET_PLAYER_ANSWER = "SET_PLAYER_ANSWER";
 export const CORRECT_ANSWER = "CORRECT_ANSWER";
+export const INCORRECT_ANSWER = "INCORRECT_ANSWER";
 
 export function updateScore(index: any, score: any) {
-    return {
-        type: UPDATE_SCORE,
-        score: score,
-        index,
-    };
+  return {
+    type: UPDATE_SCORE,
+    score: score,
+    index,
+  };
 }
 
 export function setAnswer(answer: string | undefined) {
-    return {
-        type: SET_ANSWER,
-        answer: answer,
-    };
+  return {
+    type: SET_ANSWER,
+    answer: answer,
+  };
 }
 
 export function setPlayerAnswer(answer: string | undefined) {
-    return {
-        type: SET_PLAYER_ANSWER,
-        playerAnswer: answer,
-    };
+  return {
+    type: SET_PLAYER_ANSWER,
+    playerAnswer: answer,
+  };
 }
 
 export function playerName(index: any, name: any) {
-    return {
-        type: ADD_PLAYER,
-        index,
-        name,
-    };
+  return {
+    type: ADD_PLAYER,
+    index,
+    name,
+  };
 }
 
 export function selectPlayer(index: any) {
-    return {
-        type: SELECT_PLAYER,
-        index,
-    };
+  return {
+    type: SELECT_PLAYER,
+    index,
+  };
 }
 
 export function correctAnswer(index: any) {
-    return {
-        type: CORRECT_ANSWER,
-        index,
-    };
+  return {
+    type: CORRECT_ANSWER,
+    index,
+  };
+}
+
+export function incorrectAnswer(index: any, incorrectQuestion: any) {
+  return {
+    type: INCORRECT_ANSWER,
+    index,
+    incorrectQuestion,
+  };
 }
 
 export function moveToNextQuestion() {
-    return {
-        type: NEXT_QUESTION,
-    };
+  return {
+    type: NEXT_QUESTION,
+  };
 }
