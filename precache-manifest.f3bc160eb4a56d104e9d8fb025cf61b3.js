de9187ced29ea60336d372bc204e2f34b500c2c6
Leoul Tilahun <33269725+PrinceUmbrella@users.noreply.github.com>
2020-10-23 03:23:25 -0400

    Updates

---
+++ precache-manifest.f3bc160eb4a56d104e9d8fb025cf61b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1f31ff5d8403adb0be92781218f254",
+    "url": "/triviaGameTS/index.html"
+  },
+  {
+    "revision": "6b3918d6d10986cb1ee5",
+    "url": "/triviaGameTS/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "3ac462f25c63b5748c1c",
+    "url": "/triviaGameTS/static/css/main.5e95f782.chunk.css"
+  },
+  {
+    "revision": "6b3918d6d10986cb1ee5",
+    "url": "/triviaGameTS/static/js/2.a84a7903.chunk.js"
+  },
+  {
+    "revision": "2e54b551fdc3988dd651d638460fe789",
+    "url": "/triviaGameTS/static/js/2.a84a7903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac462f25c63b5748c1c",
+    "url": "/triviaGameTS/static/js/main.a426c9b0.chunk.js"
+  },
+  {
+    "revision": "6edc238aa79a873feb06",
+    "url": "/triviaGameTS/static/js/runtime-main.30475afc.js"
+  },
+  {
+    "revision": "0a5e27d5dd4babaf78cd084494d728b7",
+    "url": "/triviaGameTS/static/media/background.0a5e27d5.jpg"
+  }
+]);
